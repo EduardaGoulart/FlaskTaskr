3ef4370dd190f637d8567ab887bdbbc7e6fa9f0f
Eduarda Goulart <goularteduarda.a@gmail.com>
2018-01-04 11:52:39 -0600

    Dynamic Route

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#any change in code will be auto loaded
+#You do not have to restart your server to refresh the changes
+app.config["DEBUG"] = True
+
 @app.route("/")
 @app.route("/hello")
 def hello_world():
@@ -11,6 +15,7 @@ def hello_world():
 def search(search_query):
     return search_query
 
+#Static Route
 @app.route("/integer/<int:value>")
 def int_type(value):
     print(value + 1)
@@ -21,10 +26,18 @@ def float_type(value):
     print(value + 1)
     return "correct"
 
+#Dynamic
 @app.route("/path/<path:value>")
 def path_type(value):
     print(value)
     return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "eduarda":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 if __name__ == "__main__":
     app.run()
